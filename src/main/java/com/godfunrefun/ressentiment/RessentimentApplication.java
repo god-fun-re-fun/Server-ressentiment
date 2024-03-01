6de2af09cd375147edf512e93840002399c26889
sayishuu <smile52673@gmail.com>
2024-03-01 17:35:51 +0900

    inital setting

---
+++ src/main/java/com/godfunrefun/ressentiment/RessentimentApplication.java
@@ -0,0 +1,13 @@
+package com.godfunrefun.ressentiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessentimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessentimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godfunrefun/ressentiment/RessentimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godfunrefun.ressentiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessentimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
